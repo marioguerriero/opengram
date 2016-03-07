827d549b9b857bd1bd7c27794beb4906a9bd3afe
marioguerriero <marioguerriero33@gmail.com>
2016-03-07 18:10:56 +0100

    fixed mongodb connection uri

--- src/db.js
+++ src/db.js
@@ -2,5 +2,9 @@ var config = require("./config");
 var mongoose = require("mongoose");
 
 module.exports.init = function() {
-  mongoose.connect("mongodb://localhost/" + config.db);
+  var connection_string = 'mongodb://127.0.0.1:27017/' + config.db;
+  if(process.env.OPENSHIFT_MONGODB_DB_URL){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.db;
+  }
+  mongoose.connect(connection_string);
 };
