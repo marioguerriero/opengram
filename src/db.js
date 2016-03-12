4ea962ba600bb8d182c34639967a754b38c2b2c9
marioguerriero <marioguerriero33@gmail.com>
2016-03-12 19:32:28 +0100

    reverted useless changes from previous commit

--- src/db.js
+++ src/db.js
@@ -2,12 +2,9 @@ var config = require("./config");
 var mongoose = require("mongoose");
 
 module.exports.init = function() {
-  //var connection_string = 'mongodb://127.0.0.1:27017/' + config.db;
-  //if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  //  connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.db;
-  //}
-  //else if(process.env.HEROKU) {
-    connection_string =  "mongodb://mario:mario@ds011459.mlab.com:11459/social-network";
-  //}
+  var connection_string = 'mongodb://127.0.0.1:27017/' + config.db;
+  if(process.env.OPENSHIFT_MONGODB_DB_URL){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.db;
+  }
   mongoose.connect(connection_string);
 };
