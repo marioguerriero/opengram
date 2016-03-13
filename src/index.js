35dca898be1aa346cd9e3a32fabec9d03c5e1b69
marioguerriero <marioguerriero33@gmail.com>
2016-03-13 20:21:37 +0100

    default port is now 808

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use("/assets", express.static(__dirname + "/view/assets"));
 app.use("/css", express.static(__dirname + "/view/css"));
 
 // Put the server on listening status
-app.set("port", process.env.OPENSHIFT_NODEJS_PORT || 8081);
+app.set("port", process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set("ip", process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 app.listen(app.get("port"), app.get("ip"), function() {
