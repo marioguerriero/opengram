805437ff32699a8f64f747f5c98ba63c0b6846ed
marioguerriero <marioguerriero33@gmail.com>
2016-03-13 19:20:14 +0100

    updated openshift connection parameters

--- src/db.js
+++ src/db.js
@@ -2,9 +2,6 @@ var config = require("./config");
 var mongoose = require("mongoose");
 
 module.exports.init = function() {
-  var connection_string = 'mongodb://127.0.0.1:27017/' + config.db;
-  if(process.env.OPENSHIFT_MONGODB_DB_URL){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.db;
-  }
+  var connection_string = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://127.0.0.1:27017/" + config.db;
   mongoose.connect(connection_string);
 };
