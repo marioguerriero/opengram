5cf112ca186f419bcf976bedf2cfc5b363a404a2
marioguerriero <marioguerriero33@gmail.com>
2016-08-17 12:07:37 +0200

    change database host environmental variable name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'TWFyaW8gR3VlcnJpZXJvIGNyZWF0ZWQgdGhpcyBzb2NpYWwgbmV0d29yaw==',
-    dbhost: process.env.MONGOLAB || 'mongodb://localhost'
+    dbhost: process.env.DBHOST || 'mongodb://localhost'
 };
