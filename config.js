9f95d8c3cef3bf7b6f6559ca3203135027bc27e8
marioguerriero <marioguerriero33@gmail.com>
2016-09-06 18:14:32 +0200

    change mongodb config var name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'TWFyaW8gR3VlcnJpZXJvIGNyZWF0ZWQgdGhpcyBzb2NpYWwgbmV0d29yaw==',
-    dbhost: process.env.DBHOST || 'mongodb://localhost'
+    dbhost: process.env.PROD_MONGODB || 'mongodb://localhost'
 };
