8d0e5a59b1c2fdf8c84f3021745c16e5b6543330
marioguerriero <marioguerriero33@gmail.com>
2016-12-21 18:36:36 +0100

    if a user which is already logged in tries to access to register or login resources then redirect to home

--- component_routes.js
+++ component_routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
+import { homeRedirect } from './components/RouteHandlers'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import UserPage from './components/UserPage';
@@ -9,11 +10,13 @@ import NotFoundPage from './components/NotFoundPage';
 import RegisterPage from './components/RegisterPage';
 import LoginPage from './components/LoginPage';
 
+
+
 export default (
     <Route path="/" component={Layout}>
         <IndexRoute component={IndexPage}/>
-        <Route path="register" component={RegisterPage}/>
-        <Route path="login" component={LoginPage}/>
+        <Route path="register" component={RegisterPage} onEnter={homeRedirect} />
+        <Route path="login" component={LoginPage} onEnter={homeRedirect} />
         <Route path="user/:id" component={UserPage}/>
         <Route path="post/:id" component={PostPage}/>
         <Route path="*" component={NotFoundPage}/>
