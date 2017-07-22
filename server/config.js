4d443d7ba661077ab3178472c72930f1825ba90d
Mario Guerriero <marioguerriero33@gmail.com>
2017-07-22 11:13:04 +0200

    fix config.js for heroku deploy

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
-export default {
+const config = {
     secret: 'TWFyaW8gR3VlcnJpZXJvIGNyZWF0ZWQgdGhpcyBzb2NpYWwgbmV0d29yaw==',
     dbhost: process.env.PROD_MONGODB || 'mongodb://localhost:27017',
     dbname: 'opengram'
 };
+
+export default config;
